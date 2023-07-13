f4b3c7c50ebd681b9436dc165575248ab62f293b
Dumbalaone <dumbala149@gmail.com>
2023-07-13 08:51:21 -0700

    checker checks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,15 @@
  */
 int main(void)
 {
-char a; int b; long int c; long long int d; float f;
-printf("size of char a: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of int b: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of long long int: %lu bytes(s)\n",(unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size of a char a: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("size of a int b: %lu byte(s)\n",(unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }
