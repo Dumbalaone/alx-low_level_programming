037e12cbeec378d3b4d20c316007967ee43f77cc
Dumbalaone <dumbala149@gmail.com>
2023-07-13 09:01:31 -0700

    vim6 file error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints the size of various computer types
+ * main - A program that prints the size of various computer types
  * Return 0 (success)
  */
 int main(void)
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char a: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of a int b: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("size of a char a: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a int b: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
