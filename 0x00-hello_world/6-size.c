a86053721c80d62a01f968ac07fda1d35046488e
Dumbalaone <dumbala149@gmail.com>
2023-07-13 07:01:59 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/** main - prints the size of various types on the computer
+ * Return: 0 - (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("size of a char a: %lu byte(s)\n"(unsigned long)sizeof(a));
+printf("size of int: %lu bytes(s)\n"(unsigned long)sizeof(b));
+printf("size of long int: %lu bytes(s)\n"(unsigned long)sizeof(c));
+printf("size of long long int: %lu bytes(s)\n"(unsigned long)sizeof(d));
+printf("size of float: %lu bytes(s)\n"(unsigned long)sizeof(f));
+return (0);
+}
