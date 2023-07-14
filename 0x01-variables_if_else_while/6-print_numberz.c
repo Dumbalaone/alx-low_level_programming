a4198d782b71e083e772843b513b041e7f986afb
Dumbalaone <dumbala149@gmail.com>
2023-07-14 11:02:36 -0700

    numberzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (number = 0; number <= 9; num++)
+	{
+		putchar(number + '0');
+	}
+	putchar('\n');
+	return (0);
+}
