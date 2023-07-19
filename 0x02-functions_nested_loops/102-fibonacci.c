095ed9411783e99c5095495f76486269543f3f9e
Dumbalaone <dumbala149@gmail.com>
2023-07-19 04:20:27 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the progam
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long prev_num = 1, current_num = 2, temp;
+
+	int i;
+
+	printf("%lu", "%lu", prev_num, current_num);
+
+	for (i = 2; i < 50; ++i)
+	{
+		temp = prev_num + current_num;
+		printf(", %lu", temp);
+		prev_num = current_num;
+		current_num = temp;
+	}
+	printf("\n");
+	return 0;
+}
