2d1297aada97a8c1a4b976d6a8df5672a863884c
Dumbalaone <dumbala149@gmail.com>
2023-07-19 03:45:08 -0700

    nightprod

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_times_table - Print the times of the input
+ * print_times_table - Prints the timetable of the input
  * starting with 0
  * @n: The value of the times table to be printed
  */
@@ -16,15 +16,18 @@ void print_times_table(int n)
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
+
 				_putchar(' ');
+
 				prod = num * mult;
-				if (prod <= 90)
+				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
+
 					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
