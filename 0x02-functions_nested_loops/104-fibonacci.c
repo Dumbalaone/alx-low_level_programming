0547f77197c187577e1381a03eed35f9da616faa
Dumbalaone <dumbala149@gmail.com>
2023-07-19 05:24:52 -0700

    fibbo104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,59 @@
-void print_number(unsigned long num);
+#include <unistd.h>
+
+void print_number(unsigned long long num);
+
 /**
  * main - Entry point of the program
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	unsigned long long first = 1, second = 2, next;
-
 	int i;
 
 	print_number(first);
-
+	write(1, ", ", 2);
 	print_number(second);
 
 	for (i = 2; i < 98; ++i)
 	{
 		next = first + second;
-		printf(", ");
+		write(1, ", ", 2);
 		print_number(next);
 		first = second;
 		second = next;
 	}
+
+	write(1, "\n", 1);
 	return (0);
 }
+
 /**
  * print_number - Custom function to print a number
+ *
  * @num: The number to print
  */
 void print_number(unsigned long long num)
 {
-	unsigned long long divider = 1;
-
-	unsigned long long copy = num;
-
-	while (divider > 0)
+	if (num == 0)
 	{
-		char digit = '0' + num / divider;
+		write(1, "0", 1);
+		return;
+	}
 
-		putchar(digit);
-		divider /= 10;
+	char buffer[21];
+	int index = 0;
+
+	while (num > 0)
+	{
+		buffer[index++] = '0' + num % 10;
+		num /= 10;
+	}
+
+	for (int i = index - 1; i >= 0; i--)
+	{
+		write(1, &buffer[i], 1);
 	}
 }
-
 
