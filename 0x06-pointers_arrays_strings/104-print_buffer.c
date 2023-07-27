9aaf97fc5d5b9c228bf3a243189a8aded2236862
Dumbalaone <dumbala149@gmail.com>
2023-07-27 13:11:44 -0700

    dont play with betty

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,9 +8,11 @@
  */
 void print_buffer(char *b, int size)
 {
+
 	int o, j, i;
-	
+
 	o = 0;
+
 	if (size <= 0)
 	{
 		printf("\n");
@@ -20,20 +22,21 @@ void print_buffer(char *b, int size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x:", o);
-			for (i = 0; i < 10; i++)
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%2x", *(b + o + i));
+			else
+				printf(" ");
+			if (i % 2)
 			{
-				if (i < j)
-					printf("%2x", *(b + o + i));
-				else
-					printf(" ");
-				if(i % 2)
-				{
-					printf(" ");
-				}
+				printf(" ");
 			}
-		for(i = 0; i < j; i++)
+		}
+		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
+
 			if (c < 32 || c > 132)
 			{
 				c = '.';
